ec236f1690b8e92362d3dc8570349e494de3fc3f
Kai Siang <peter82615@yahoo.com.tw>
2024-08-26 23:50:50 +0800

    設定todo資料庫完成

--- assignment/models.py
+++ assignment/models.py
@@ -1,3 +1,11 @@
-from django.db import models
+from django.db import models, connection
 
 # Create your models here.
+class Todo(models.Model):
+    todo_id = models.AutoField(primary_key=True)
+    completed = models.BooleanField()
+    todo =  models.CharField(max_length=100)
+    last_uplate = models.DateTimeField()
+
+    class Meta:
+        db_table = "Todo"
